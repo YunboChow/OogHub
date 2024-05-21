c2486e6f60e6c6128afe0bff9dd83ffcd9b12ce0
Max <max.svedala@gmail.com>
2024-05-21 14:59:04 +0200

    Fixed with the api calls so know we render based on the quotes and added a file to gitignore

---
+++ Client/src/api/quoteAPI.js
@@ -0,0 +1,22 @@
+//todo: Change limit later 
+
+export const quoteApi = () => {
+  const getQuotes = async () => {
+    try{
+      const response = await fetch('https://api.quotable.io/quotes/random?limit=34');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const items = await response.json();
+      return items;
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  return{
+    getQuotes,
+  };
+}
+
+
