242d1de7ba75d329638cb5eb2193824f7214fa86
YunboChow <96528637+YunboChow@users.noreply.github.com>
2024-05-28 18:13:14 +0200

    Removed classname from buttons

--- Client/src/Components/Nav.jsx
+++ Client/src/Components/Nav.jsx
@@ -18,10 +18,9 @@ function Nav({refresh,setQuotes, setRefreshedQuotes}) {
                 <Flex flexDir={'row'} justifyContent={'end'} alignItems={'center'} h="100%">
                 <Text alignContent={'center'} h={'100%'} fontSize={'30px'} ml={'10px'} width={'100%'}>OOGHUB</Text>
                     {/* TODO: REMOVE THIS BUTTON AND MAKE TEXT CLICKABLE */}
-                    <Button as={motion.div} className="refresh" variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={()=>setQuotes(JSON.parse(localStorage.getItem('previousQuotes')) || [])}  bgColor={'transparent'}>HOME</Button>
-                
-                    <Button as={motion.div} className="refresh" variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={()=>refresh()}>Refresh</Button>
-                    <Button as={motion.div} className="refresh" variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={()=>{setQuotes(JSON.parse(localStorage.getItem('savedQuotes')) || []); setRefreshedQuotes(false)}}>Liked</Button>
+                    <Button as={motion.div} variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={()=>setQuotes(JSON.parse(localStorage.getItem('previousQuotes')) || [])}  bgColor={'transparent'}>HOME</Button>
+                    <Button as={motion.div} variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={()=>refresh()}>Refresh</Button>
+                    <Button as={motion.div} variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={()=>{setQuotes(JSON.parse(localStorage.getItem('savedQuotes')) || []); setRefreshedQuotes(false)}}>Liked</Button>
                     <IconButton onClick={()=>setDarkMode(!darkMode)} mr={'20px'} icon={darkMode === false ? <SunIcon /> : <MoonIcon />}/>
                 </Flex>
             </Box>
