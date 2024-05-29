0af87d1eb6c2aa651c5736ffd7e047fef0157862
Max <max.svedala@gmail.com>
2024-05-29 12:01:13 +0200

    Removed griditem hover effect

--- Client/src/Components/QuoteBox.jsx
+++ Client/src/Components/QuoteBox.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import { Flex, Text } from "@chakra-ui/react";
 import SaveQuoteButton from './SaveQuote';
 import MoreInformationButton from './QuoteInformation';
-import { AnimatePresence, motion } from "framer-motion";
-import { CanvasRevealEffect } from "./ui/canvas-reveal-effect"; 
 
 function QuoteBox({ quote }) {
     const [hovered, setHovered] = useState(false);
@@ -27,26 +25,6 @@ function QuoteBox({ quote }) {
             overflow="hidden" 
             zIndex={hovered ? 10 : 1}
         >
-            <AnimatePresence>
-                {hovered && (
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        className="h-full w-full absolute inset-0 z-0" 
-                    >
-                        <CanvasRevealEffect
-                            animationSpeed={0.4}
-                            opacities={[0.3, 0.3, 0.3, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8, 1]}
-                            colors={[[0, 255, 255]]}
-                            dotSize={3}
-                            showGradient={true}
-                        />
-                    </motion.div>
-                )}
-            </AnimatePresence>
-            
-            
             <MoreInformationButton quote={quote} style={{ zIndex: 20 }} />
             <Text color={'white'} fontSize={'20px'} style={{ position: 'absolute', zIndex: 20 }}>{quote.content}</Text>
             <Text color={'grey'} fontSize={'15px'} style={{  zIndex: 20 }}>- {quote.author}</Text>
