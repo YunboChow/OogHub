12a0fa52b117eb2554e8c85cb4e0743bf2a839b9
kristoffer leal <89843271+DonLealGUA@users.noreply.github.com>
2024-05-29 09:59:54 +0200

    Added the Ooghub logo

--- Client/src/Components/Nav.jsx
+++ Client/src/Components/Nav.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, IconButton, color,Text } from '@chakra-ui/react';
+import { Box, Button, Flex, IconButton, color,Text, Image } from '@chakra-ui/react';
 import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 import React, { useState, useEffect } from "react";
 
@@ -8,10 +8,10 @@ function Nav({refresh,setQuotes, setRefreshedQuotes}) {
         <>
             <Box bgColor={'#BEAFFC'} w={'100%'} minH={'40px'} h="5%" mb={'1%'} bgGradient={'linear(to-r,blue.400,purple.400,,pink.700)'}>
                 <Flex flexDir={'row'} justifyContent={'end'} alignItems={'center'} h="100%">
-                <Text alignContent={'center'} h={'100%'} fontSize={'30px'} ml={'10px'} width={'100%'}>OOGHUB</Text>
+                <Image src={window.location.origin + '/assets/TheOog.png'} alt="Oog"  paddingLeft={"10px"} maxHeight="50px" onClick={()=>setQuotes(JSON.parse(localStorage.getItem('previousQuotes')) || [])} cursor={"pointer"}/> 
+                <Text alignContent={'center'} h={'100%'} fontSize={'30px'} ml={'10px'} width={'100%'} fontWeight={"500"}>OOGHUB</Text>
                     {/* TODO: REMOVE THIS BUTTON AND MAKE TEXT CLICKABLE */}
-                    <Button mr={'40px'} onClick={()=>setQuotes(JSON.parse(localStorage.getItem('previousQuotes')) || [])}  bgColor={'transparent'}>HOME</Button>
-                
+                    <Button mr={'40px'} onClick={()=>setQuotes(JSON.parse(localStorage.getItem('previousQuotes')) || [])}  bgColor={'transparent'}>HOME</Button>  
                     <Button mr={'40px'} onClick={()=>refresh()}>Refresh</Button>
                     <Button mr={'40px'} onClick={()=>{setQuotes(JSON.parse(localStorage.getItem('savedQuotes')) || []); setRefreshedQuotes(false)}}>Liked</Button>
                     <IconButton onClick={()=>setDarkMode(!darkMode)} mr={'20px'} icon={darkMode === false ? <SunIcon /> : <MoonIcon />}/>
