cfa56736b1e6ef69b308f9a73c6f07cf3fe89594
Max <max.svedala@gmail.com>
2024-05-29 11:18:30 +0200

    Implemented card animation and and website opening animation

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect} from 'react';
 import { Box } from '@chakra-ui/react';
 import QuoteGrid from './Components/QuoteGrid.jsx';
 import Nav from './Components/Nav.jsx';
 import { quoteApi } from './api/quoteAPI';
+import img from './assets/The_Oog.png';
+import { motion} from 'framer-motion';
 
 function App() {
   const { getQuotes } = quoteApi();
@@ -14,9 +16,10 @@ function App() {
     if (!quotes) {
       retieveQuotes();
     }
-    if(refreshedQuotes){
+    if (refreshedQuotes) {
       localStorage.setItem('previousQuotes', JSON.stringify(quotes));
     }
+    console.log(quotes);
   }, [quotes]);
 
   const retieveQuotes = async () => {
@@ -27,10 +30,31 @@ function App() {
   }
 
   return (
-    <Box w='100vw' h='100vh' overflow='auto' background='radial-gradient(circle, #25273D, #010316)'>
-      <Nav refresh={retieveQuotes} setQuotes={setQuotes} setRefreshedQuotes={setRefreshedQuotes}></Nav>
-      {loading && ( 
-        <QuoteGrid quotes={quotes}/>
+    <Box w='100vw' h='100vh' overflow='auto' background='radial-gradient(circle, #25273D, #010316)'  zIndex={22} >
+      <Nav refresh={retieveQuotes} setQuotes={setQuotes} setRefreshedQuotes={setRefreshedQuotes}/>
+      {loading && (
+        <>
+          <motion.div
+            variants={{
+              hidden: { left: '5%' },
+              visible: { left: '100%' },
+            }}
+            initial='hidden'
+            animate='visible'
+            transition={{ duration: 4, delay: 1 }}
+            style={{
+              position: 'absolute',
+              top: 4,
+              bottom: 4,
+              left: 0,
+              right: 0,
+              backgroundImage: `url(${img})`,
+              backgroundSize: 'cover',
+              zIndex: 21,
+            }}
+          />
+          <QuoteGrid quotes={quotes} />
+        </>
       )}
     </Box>
   );
--- Client/src/Components/Nav.jsx
+++ Client/src/Components/Nav.jsx
@@ -1,27 +1,27 @@
-import { Box, Button, Flex, IconButton, color,Text } from '@chakra-ui/react';
+import { Box, Button, Flex, IconButton, color, Text } from '@chakra-ui/react';
 import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 import React, { useState, useEffect } from "react";
 import { motion } from 'framer-motion'
 
-function Nav({refresh,setQuotes, setRefreshedQuotes}) {
+function Nav({ refresh, setQuotes, setRefreshedQuotes }) {
     const [darkMode, setDarkMode] = useState(true);
 
     const button = {
         rest: { scale: 1 },
         hover: { scale: 1.1 },
         pressed: { scale: 0.95 }
-      };
+    };
 
     return (
         <>
             <Box bgColor={'#BEAFFC'} w={'100%'} minH={'40px'} h="5%" mb={'1%'} bgGradient={'linear(to-r,blue.400,purple.400,,pink.700)'}>
                 <Flex flexDir={'row'} justifyContent={'end'} alignItems={'center'} h="100%">
-                <Text alignContent={'center'} h={'100%'} fontSize={'30px'} ml={'10px'} width={'100%'}>OOGHUB</Text>
+                    <Text alignContent={'center'} h={'100%'} fontSize={'30px'} ml={'10px'} width={'100%'}>OOGHUB</Text>
                     {/* TODO: REMOVE THIS BUTTON AND MAKE TEXT CLICKABLE */}
-                    <Button as={motion.div} variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={()=>setQuotes(JSON.parse(localStorage.getItem('previousQuotes')) || [])}  bgColor={'transparent'}>HOME</Button>
-                    <Button as={motion.div} variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={()=>refresh()}>Refresh</Button>
-                    <Button as={motion.div} variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={()=>{setQuotes(JSON.parse(localStorage.getItem('savedQuotes')) || []); setRefreshedQuotes(false)}}>Liked</Button>
-                    <IconButton onClick={()=>setDarkMode(!darkMode)} mr={'20px'} icon={darkMode === false ? <SunIcon /> : <MoonIcon />}/>
+                    <Button as={motion.div} variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={() => setQuotes(JSON.parse(localStorage.getItem('previousQuotes')) || [])} bgColor={'transparent'}>HOME</Button>
+                    <Button as={motion.div} variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={() => refresh()}>Refresh</Button>
+                    <Button as={motion.div} variants={button} initial="rest" whileHover="hover" whileTap="pressed" mr={'40px'} onClick={() => { setQuotes(JSON.parse(localStorage.getItem('savedQuotes')) || []); setRefreshedQuotes(false) }}>Liked</Button>
+                    <IconButton onClick={() => setDarkMode(!darkMode)} mr={'20px'} icon={darkMode === false ? <SunIcon /> : <MoonIcon />} />
                 </Flex>
             </Box>
         </>
